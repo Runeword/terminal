5d3a07b656de32e2a0d450adfe03a3402e6eb312
Runeword <60324746+Runeword@users.noreply.github.com>
2025-06-17 22:08:44 +0200

    Add config/shell/functions/utilities.sh

---
+++ config/shell/functions/utilities.sh
@@ -0,0 +1,9 @@
+pwd() {
+  command pwd | tee /dev/tty | {
+    if command -v pbcopy >/dev/null 2>&1; then
+      pbcopy
+    elif command -v wl-copy >/dev/null 2>&1; then
+      wl-copy
+    fi
+  }
+}
