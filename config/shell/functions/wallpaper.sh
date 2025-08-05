754522f98667515fba11bcdbf365ebb58b81302c
Runeword <60324746+Runeword@users.noreply.github.com>
2025-08-05 23:43:25 +0200

    Update config/shell/functions/wallpaper.sh

--- config/shell/functions/wallpaper.sh
+++ config/shell/functions/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 __wallpaper() {
-	hyprctl hyprpaper preload "$1" &&
-		hyprctl hyprpaper wallpaper eDP-1,"$1" &&
-		hyprctl hyprpaper unload all
+  hyprctl hyprpaper preload "$1" &&
+    hyprctl hyprpaper wallpaper eDP-1,"$1" &&
+    hyprctl hyprpaper unload all
 }
