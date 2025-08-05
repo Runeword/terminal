0d55a79aed5d350f2c09be8d10f65aa77b0c3013
Runeword <60324746+Runeword@users.noreply.github.com>
2025-08-05 23:43:25 +0200

    Update config/shell/scripts/ssh-agent.sh

--- config/shell/scripts/ssh-agent.sh
+++ config/shell/scripts/ssh-agent.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if ! pgrep -u "$USER" ssh-agent >/dev/null; then
-	ssh-agent -t 2h >"$XDG_RUNTIME_DIR/ssh-agent.env"
+  ssh-agent -t 2h >"$XDG_RUNTIME_DIR/ssh-agent.env"
 fi
-if [ -z "$SSH_AUTH_SOCK" ]; then
-	. "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+if [ "$SSH_AUTH_SOCK" = "" ]; then
+  . "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
 fi
