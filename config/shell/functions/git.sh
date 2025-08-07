ac85774bd220d2071e1dcd8c2bacc3fcdce1285c
Runeword <60324746+Runeword@users.noreply.github.com>
2025-08-07 23:55:22 +0200

    Update config/shell/functions/git.sh

--- config/shell/functions/git.sh
+++ config/shell/functions/git.sh
@@ -5,7 +5,7 @@ __git_clone() {
   local base_dir="${HOME}/${1}"
   mkdir -p "$base_dir" # Create the base directory if it doesn't exist
   git clone "$repo_url" "$base_dir/$(basename "$repo_url" .git)"
-  cd "$base_dir/$(basename "$repo_url" .git)" || return # Change into the cloned directory
+  builtin cd "$base_dir/$(basename "$repo_url" .git)" || return # Change into the cloned directory
 }
 
 __git_open_url() {
@@ -44,7 +44,7 @@ __git_fzf_cmd() {
     fzf_args="$default_args"
   fi
 
-  (cd "$repo_root" && eval "$list_cmd") |
+  (builtin cd "$repo_root" && eval "$list_cmd") |
     eval "fzf $fzf_args" |
     sed "s|^|$repo_root/|" |
     xargs -r sh -c "$action_cmd \"\$@\"" _
