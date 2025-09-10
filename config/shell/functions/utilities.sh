51c8ee6b5162c8eb1ccb0537836839aa3b4a209d
Runeword <60324746+Runeword@users.noreply.github.com>
2025-09-10 22:57:21 +0200

    Update config/shell/functions/utilities.sh

--- config/shell/functions/utilities.sh
+++ config/shell/functions/utilities.sh
@@ -1,9 +1,9 @@
-pwd() {
-  builtin pwd | tee /dev/tty | {
-    if command -v pbcopy >/dev/null 2>&1; then
-      pbcopy
-    elif command -v wl-copy >/dev/null 2>&1; then
-      wl-copy
-    fi
-  }
-}
+# pwd() {
+#   builtin pwd | tee /dev/tty | {
+#     if command -v pbcopy >/dev/null 2>&1; then
+#       pbcopy
+#     elif command -v wl-copy >/dev/null 2>&1; then
+#       wl-copy
+#     fi
+#   }
+# }
