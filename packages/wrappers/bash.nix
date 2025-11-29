0ba486e72cdce51ca096448c8441210bbe843047
Runeword <60324746+Runeword@users.noreply.github.com>
2025-11-29 09:29:41 +0100

    Update packages/wrappers/bash.nix

--- packages/wrappers/bash.nix
+++ packages/wrappers/bash.nix
@@ -8,10 +8,12 @@ pkgs.symlinkJoin {
     ${pkgs.lib.mkLink "bash/bashrc" ".config/bash/.bashrc"}
     ${pkgs.lib.mkLink "shell" ".config/shell"}
     ${pkgs.lib.mkLink "readline" ".config/readline"}
+    ${pkgs.lib.mkLink "direnv" ".config/direnv"}
 
     wrapProgram $out/bin/bash \
       --add-flags "--rcfile $out/.config/bash/.bashrc" \
       --set OUT "$out" \
-      --set INPUTRC "$out/.config/readline/inputrc"
+      --set INPUTRC "$out/.config/readline/inputrc" \
+      --set DIRENV_CONFIG "$out/.config/direnv"
   '';
 }
