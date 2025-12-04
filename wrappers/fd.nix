5369a2043aa810583a0adbac53d05b43c151c990
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-04 23:04:35 +0100

    Update wrappers/fd.nix

--- wrappers/fd.nix
+++ wrappers/fd.nix
@@ -1,11 +1,18 @@
-{ pkgs }:
+{ pkgs, useLink ? false, configRoot ? ../config }:
+
+let
+  mkConfig = path: target:
+    if useLink
+    then pkgs.lib.mkLink "config/${path}" target
+    else pkgs.lib.mkCopy "${configRoot}/${path}" target;
+in
 
 pkgs.symlinkJoin {
   name = "fd-with-config";
   paths = [ pkgs.fd ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${pkgs.lib.mkLink "ignore" ".config/ignore"}
+    ${mkConfig "ignore" ".config/ignore"}
 
     wrapProgram $out/bin/fd \
       --add-flags "--ignore-file $out/.config/ignore"
