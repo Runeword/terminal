e45edba9c4a6ea988a37fd5ce0cdc7b800c557ce
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-04 23:04:35 +0100

    Update wrappers/tmux.nix

--- wrappers/tmux.nix
+++ wrappers/tmux.nix
@@ -1,7 +1,12 @@
-{ pkgs }:
+{ pkgs, useLink ? false, configRoot ? ../config }:
 
 let
-  zsh = import ./zsh.nix { inherit pkgs; };
+  zsh = import ./zsh.nix { inherit pkgs useLink configRoot; };
+
+  mkConfig = path: target:
+    if useLink
+    then pkgs.lib.mkLink "config/${path}" target
+    else pkgs.lib.mkCopy "${configRoot}/${path}" target;
 in
 
 pkgs.symlinkJoin {
@@ -9,9 +14,9 @@ pkgs.symlinkJoin {
   paths = [ pkgs.tmux ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${pkgs.lib.mkLink "tmux/tmux.conf" ".config/tmux/tmux.conf"}
-    ${pkgs.lib.mkLink "shell/functions/tmux.sh" ".config/shell/functions/tmux.sh"}
-    ${pkgs.lib.mkLink "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
+    ${mkConfig "tmux/tmux.conf" ".config/tmux/tmux.conf"}
+    ${mkConfig "shell/functions/tmux.sh" ".config/shell/functions/tmux.sh"}
+    ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
     wrapProgram $out/bin/tmux \
     --set TMUX_SHELL ${zsh}/bin/zsh \
