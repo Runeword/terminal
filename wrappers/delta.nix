47d9aa8f5d1540c9dda2087f48833b6e3181c814
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-04 23:04:35 +0100

    Update wrappers/delta.nix

--- wrappers/delta.nix
+++ wrappers/delta.nix
@@ -1,11 +1,18 @@
-{ pkgs }:
+{ pkgs, useLink ? false, configRoot ? ../config }:
+
+let
+  mkConfig = path: target:
+    if useLink
+    then pkgs.lib.mkLink "config/${path}" target
+    else pkgs.lib.mkCopy "${configRoot}/${path}" target;
+in
 
 pkgs.symlinkJoin {
   name = "delta-with-config";
   paths = [ pkgs.delta ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${pkgs.lib.mkLink "delta/config" ".config/delta/config"}
+    ${mkConfig "delta/config" ".config/delta/config"}
 
     wrapProgram $out/bin/delta \
       --add-flags "--config $out/.config/delta/config"
