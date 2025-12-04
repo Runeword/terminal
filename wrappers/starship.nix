20b85cf6d03a5eb241623e7049e8b51d993969c8
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-04 23:04:35 +0100

    Update wrappers/starship.nix

--- wrappers/starship.nix
+++ wrappers/starship.nix
@@ -1,11 +1,18 @@
-{ pkgs }:
+{ pkgs, useLink ? false, configRoot ? ../config }:
+
+let
+  mkConfig = path: target:
+    if useLink
+    then pkgs.lib.mkLink "config/${path}" target
+    else pkgs.lib.mkCopy "${configRoot}/${path}" target;
+in
 
 pkgs.symlinkJoin {
   name = "starship-with-config";
   paths = [ pkgs.starship ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${pkgs.lib.mkLink "starship.toml" ".config/starship.toml"}
+    ${mkConfig "starship.toml" ".config/starship.toml"}
 
     wrapProgram $out/bin/starship \
       --set STARSHIP_CONFIG "$out/.config/starship.toml"
