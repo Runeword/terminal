dc6196332f8f37592522aa3cf5d90cbdbd3849fd
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-09 22:12:58 +0100

    Update config/shell/functions/utilities.sh

--- config/shell/functions/utilities.sh
+++ config/shell/functions/utilities.sh
@@ -7,3 +7,19 @@
 #     fi
 #   }
 # }
+
+__paste() {
+  if command -v pbpaste >/dev/null 2>&1; then
+    pbpaste
+  elif command -v wl-paste >/dev/null 2>&1; then
+    wl-paste
+  fi
+}
+
+__copy() {
+  if command -v pbcopy >/dev/null 2>&1; then
+    pbcopy
+  elif command -v wl-copy >/dev/null 2>&1; then
+    wl-copy
+  fi
+}
