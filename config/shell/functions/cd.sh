c663a6c7ec4a9933cafcba18f7c8d2634923b7e2
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-13 22:09:15 +0100

    Update config/shell/functions/cd.sh

--- config/shell/functions/cd.sh
+++ config/shell/functions/cd.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 __cd() {
-  local target="$@"
-  if [[ -z "$target" ]]; then
+  local target="$*"
+  if [ -z "$target" ]; then
     # Handle 'cd' without arguments; change to the $HOME directory.
     target="$HOME"
   fi
@@ -8,16 +10,16 @@ __cd() {
   # Note, if the target directory is the same as the current directory
   # do nothing since we don't want to needlessly populate the directory stack
   # with repeat entries.
-  if [[ "$target" != "$PWD" ]]; then
-    \builtin pushd "$target" 1>/dev/null
+  if [ "$target" != "$PWD" ]; then
+    builtin pushd "$target" >/dev/null 2>&1 || return 1
     printf '\033[38;2;83;94;115m%s\033[0m\n' "$PWD"
   fi
 }
 
 __nextd() {
-  pushd -0 &>/dev/null
+  pushd -0 >/dev/null 2>&1 || return 1
 }
 
 __prevd() {
-  pushd +1 &>/dev/null
+  pushd +1 >/dev/null 2>&1 || return 1
 }
