e8e74c70c48d276f81dec80fa559687a7a641a08
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-17 21:49:53 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -166,29 +166,7 @@
         apps.dev.program = "${alacritty-dev { }}/bin/alacritty";
         packages.dev = alacritty-dev { };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nixfmt-rfc-style
-            pkgs.shfmt
-            pkgs.shellharden
-            pkgs.shellcheck
-            pkgs.taplo
-            (pkgs.writeShellScriptBin "dev" ''
-              TERMINAL_CONFIG_DIR="$PWD/config" nix run .#dev --impure "$@"
-            '')
-            (pkgs.writeShellScriptBin "h" ''
-              echo "type 'dev' to run alacritty in development mode"
-              echo "type 'bdl' to run alacritty in bundled mode"
-              echo "type 'h' for help"
-            '')
-            (pkgs.writeShellScriptBin "bdl" ''
-              nix run . "$@"
-            '')
-          ];
-          shellHook = ''
-            h
-          '';
-        };
+        devShells.default = import ./devshell.nix { inherit pkgs; };
       }
     );
 }
