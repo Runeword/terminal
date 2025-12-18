e52b2c4cde51f4c2ff939947ff924044f0561e4a
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-18 22:45:50 +0100

    Update wrappers/bash.nix

--- wrappers/bash.nix
+++ wrappers/bash.nix
@@ -1,14 +1,14 @@
-{ pkgs, mkConfig }:
+{ pkgs }:
 
 pkgs.symlinkJoin {
   name = "bash-with-config";
   paths = [ pkgs.bash ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${mkConfig "bash/bashrc" ".config/bash/.bashrc"}
-    ${mkConfig "shell" ".config/shell"}
-    ${mkConfig "readline" ".config/readline"}
-    ${mkConfig "direnv" ".config/direnv"}
+    ${pkgs.lib.mkConfig "bash/bashrc" ".config/bash/.bashrc"}
+    ${pkgs.lib.mkConfig "shell" ".config/shell"}
+    ${pkgs.lib.mkConfig "readline" ".config/readline"}
+    ${pkgs.lib.mkConfig "direnv" ".config/direnv"}
 
     wrapProgram $out/bin/bash \
       --add-flags "--rcfile $out/.config/bash/.bashrc" \
