c2ea7299209d8066b4a4767c71432a1363c94b16
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-18 22:45:50 +0100

    Update wrappers/bat.nix

--- wrappers/bat.nix
+++ wrappers/bat.nix
@@ -1,11 +1,11 @@
-{ pkgs, mkConfig }:
+{ pkgs }:
 
 pkgs.symlinkJoin {
   name = "bat-with-config";
   paths = [ pkgs.bat ];
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${mkConfig "bat" ".config/bat"}
+    ${pkgs.lib.mkConfig "bat" ".config/bat"}
 
     wrapProgram $out/bin/bat \
       --set BAT_CONFIG_PATH "$out/.config/bat/config"
