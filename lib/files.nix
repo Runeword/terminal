a7facda94609b785319a1efb01d481eec22a4416
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-21 18:46:15 +0100

    Add lib/files.nix

---
+++ lib/files.nix
@@ -0,0 +1,21 @@
+{ pkgs, rootPath }:
+
+rec {
+  link = sourceStr: targetStr: ''
+    mkdir -p $(dirname $out/${pkgs.lib.escapeShellArg targetStr})
+    ln -sf ${
+      pkgs.lib.escapeShellArg (rootPath + "/" + sourceStr)
+    } $out/${pkgs.lib.escapeShellArg targetStr}
+  '';
+
+  copy = sourcePath: targetStr: ''
+    mkdir -p $(dirname $out/${pkgs.lib.escapeShellArg targetStr})
+    cp -r ${pkgs.lib.escapeShellArg (pkgs.lib.cleanSource sourcePath)} $out/${pkgs.lib.escapeShellArg targetStr}
+  '';
+
+  sync =
+    sourceStr: targetStr:
+    if !pkgs.lib.hasPrefix "/nix/store" rootPath
+    then link sourceStr targetStr
+    else copy "${rootPath}/${sourceStr}" targetStr;
+}
