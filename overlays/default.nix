f6a60b20fbe22e1d78660fd3a227605989c5e156
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-21 18:46:15 +0100

    Update overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
-{ configPath, defaultConfigRoot, pkgs-24-05 }:
+{ pkgs-24-05 }:
 [
-  (import ./lib.nix { inherit configPath defaultConfigRoot; })
   (final: prev: { awscli2 = pkgs-24-05.awscli2; })
 ]
