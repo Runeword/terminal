9ea17b5e137309893fe4b83b55a28f055d3d993f
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-22 22:44:40 +0100

    Update packages/linux.nix

--- packages/linux.nix
+++ packages/linux.nix
@@ -1,17 +1,20 @@
 { pkgs }:
 
-pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [
-  wl-clipboard # copy/paste
-  dragon-drop # drag and drop
-  nvme-cli # NVMe storage devices manager
-  hwinfo # hardware info
-  evtest # input device testing
-  libinput # input device handling
-  # ventoy-full # USB boot utility
-  dmidecode
-  cloneit
-  xarchiver
-  git-graph
-  distrobox
-  qdirstat # Disk usage viewer
-])
+pkgs.lib.optionals (pkgs.stdenv.isLinux) (
+  with pkgs;
+  [
+    wl-clipboard # copy/paste
+    dragon-drop # drag and drop
+    nvme-cli # NVMe storage devices manager
+    hwinfo # hardware info
+    evtest # input device testing
+    libinput # input device handling
+    # ventoy-full # USB boot utility
+    dmidecode
+    cloneit
+    xarchiver
+    git-graph
+    distrobox
+    qdirstat # Disk usage viewer
+  ]
+)
