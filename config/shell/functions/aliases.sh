8845c4975e1daa2a9291e4645f3d70c3e006f323
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-23 23:47:30 +0100

    Update config/shell/functions/aliases.sh

--- config/shell/functions/aliases.sh
+++ config/shell/functions/aliases.sh
@@ -3,17 +3,17 @@
 __run_alias() {
   local selected
 
-  selected=$(alias \
-    | fzf \
+  selected=$(alias |
+    fzf \
       --delimiter='=' \
       --height 70% \
       --reverse \
       --prompt='  ' \
       --no-separator \
-      --info=inline:'' \
-    | awk -F'=' '{print $2}' | sed "s/^'//;s/'$//") || return 0
+      --info=inline:'' |
+    awk -F'=' '{print $2}' | sed "s/^'//;s/'$//") || return 0
 
-    sh -c "$selected"
+  sh -c "$selected"
 }
 
 __aliases() {
@@ -23,11 +23,11 @@ __aliases() {
   # Parse arguments
   while [ $# -gt 0 ]; do
     case "$1" in
-      -p|--prefix)
+      -p | --prefix)
         prefix_char="$2"
         shift 2
         ;;
-      -f|--file)
+      -f | --file)
         aliases_file="$2"
         shift 2
         ;;
@@ -38,33 +38,33 @@ __aliases() {
   done
 
   local selected_command
-  selected_command=$(< "$aliases_file" \
-    column \
-    --table \
-    --separator $'\t' \
-    --output-separator $'\u00A0' \
-    | fzf -i \
-    --with-nth=1,2,3 \
-    --print-query \
-    --query "^" \
-    --exact \
-    --nth=1 \
-    --no-info \
-    --no-separator \
-    --delimiter=$'\u00A0' \
-    --cycle \
-    --no-preview \
-    --reverse \
-    --no-sort \
-    --prompt='  ' \
-    --bind 'one:accept,zero:accept,tab:accept' \
-    --height 70% \
+  selected_command=$(
+    <"$aliases_file" column \
+      --table \
+      --separator $'\t' \
+      --output-separator $'\u00A0' |
+      fzf -i \
+        --with-nth=1,2,3 \
+        --print-query \
+        --query "^" \
+        --exact \
+        --nth=1 \
+        --no-info \
+        --no-separator \
+        --delimiter=$'\u00A0' \
+        --cycle \
+        --no-preview \
+        --reverse \
+        --no-sort \
+        --prompt='  ' \
+        --bind 'one:accept,zero:accept,tab:accept' \
+        --height 70%
   )
 
   if [ $? -eq 0 ]; then
     local last_column=$(echo "$selected_command" | awk -F $'\u00A0' '{ if (NR==2) print $NF }')
     LBUFFER+=$(echo "$selected_command" | awk -F $'\u00A0' '{ if (NR==2) { sub(/[[:space:]]+$/, "", $2); print $2 " " } }')
-    
+
     if [ "$last_column" = "x" ]; then
       zle accept-line
     fi
