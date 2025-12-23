97ca87e99e260a1c0b00ccfb34c0051187593907
Runeword <60324746+Runeword@users.noreply.github.com>
2025-12-23 23:47:30 +0100

    Update config/shell/functions/devices.sh

--- config/shell/functions/devices.sh
+++ config/shell/functions/devices.sh
@@ -7,15 +7,16 @@ __open_device() {
   if [ "$devices" = "" ]; then return 0; fi
 
   local device
-  device=$(echo "$devices" | fzf \
-    --reverse \
-    --info=hidden \
-    --prompt='  ' \
-    --no-separator \
-    --height 70% \
-    --header="C-u unmount device" \
-    --header-first \
-    --bind='ctrl-u:reload-sync(umount /run/media/"$USER"/{})'
+  device=$(
+    echo "$devices" | fzf \
+      --reverse \
+      --info=hidden \
+      --prompt='  ' \
+      --no-separator \
+      --height 70% \
+      --header="C-u unmount device" \
+      --header-first \
+      --bind='ctrl-u:reload-sync(umount /run/media/"$USER"/{})'
   )
 
   if [ "$device" = "" ]; then return 0; fi
