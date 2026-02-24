d955e49d3b1fdeefeca83166503dbf6098e3d346
Runeword <60324746+Runeword@users.noreply.github.com>
2026-02-24 22:00:15 +0100

    Update devshell.nix

--- devshell.nix
+++ devshell.nix
@@ -6,13 +6,13 @@ pkgs.mkShell {
     pkgs.shellharden
     pkgs.shellcheck
     pkgs.taplo
-    (pkgs.writeShellScriptBin "dev" ''
-      TERMINAL_CONFIG_DIR="$PWD/config" nix run .#dev --impure "$@"
-    '')
     (pkgs.writeShellScriptBin "h" ''
       echo "type 'dev' to run alacritty in development mode"
       echo "type 'bdl' to run alacritty in bundled mode"
       echo "type 'h' for help"
+    '')
+    (pkgs.writeShellScriptBin "dev" ''
+      TERMINAL_CONFIG_DIR="$PWD/config" nix run .#dev --impure "$@"
     '')
     (pkgs.writeShellScriptBin "bdl" ''
       nix run . "$@"
