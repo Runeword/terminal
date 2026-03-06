bc66768f63c45015505cc11d4aeaa0c2d9cbe4b0
Runeword <60324746+Runeword@users.noreply.github.com>
2026-03-06 20:52:07 +0100

    Update overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,9 @@
-{ pkgs-24-05 }:
+{ pkgs-24-05, pkgs-25-11 }:
 [
-  (final: prev: { awscli2 = pkgs-24-05.awscli2; })
+  (final: prev: {
+    awscli2 = pkgs-24-05.awscli2;
+    tmux = pkgs-25-11.tmux;
+  })
   (final: prev: {
     firebase-tools = prev.firebase-tools.override {
       buildNpmPackage = prev.buildNpmPackage.override {
