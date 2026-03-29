8c5eb40c9540c8d39f619cda9b4831de22f8be25
Runeword <60324746+Runeword@users.noreply.github.com>
2026-03-29 23:32:37 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -101,14 +101,17 @@
             (import ./devshells/terminal.nix { inherit pkgs; })
             (import ./devshells/languages.nix { inherit pkgs; })
             claude.devShells.${system}.ast-grep
-            (lefthook.lib.mkShell pkgs [
-              lefthook.lib.auto-msg
-              lefthook.lib.format-nix
-              lefthook.lib.format-shell
-              lefthook.lib.format-toml
-              lefthook.lib.format-yaml
-              lefthook.lib.lint-shell
-            ])
+            (lefthook.lib.mkShell {
+              inherit pkgs;
+              modules = [
+                lefthook.lib.auto-msg
+                lefthook.lib.format-nix
+                lefthook.lib.format-shell
+                lefthook.lib.format-toml
+                lefthook.lib.format-yaml
+                lefthook.lib.lint-shell
+              ];
+            })
           ];
         };
       }
