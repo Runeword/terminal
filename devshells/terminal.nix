27f6cec09df391923306ebeaac55fdab5ce90882
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-26 22:03:13 +0200

    Update devshells/terminal.nix

--- devshells/terminal.nix
+++ devshells/terminal.nix
@@ -1,10 +1,12 @@
 { pkgs }:
 pkgs.mkShell {
   buildInputs = [
+    pkgs.nix-update
     (pkgs.writeShellScriptBin "h" ''
       echo "type 'dev' to run alacritty in development mode"
       echo "type 'bdl' to run alacritty in bundled mode"
       echo "type 'tools' <name> to run a binary from the tools package"
+      echo "type 'smoke' to run wrapper smoke tests"
       echo "type 'h' for help"
     '')
     (pkgs.writeShellScriptBin "dev" ''
@@ -15,6 +17,9 @@ pkgs.mkShell {
     '')
     (pkgs.writeShellScriptBin "tools" ''
       nix run .#tools -- "$@"
+    '')
+    (pkgs.writeShellScriptBin "smoke" ''
+      nix flake check -L --keep-going -j auto "$@"
     '')
   ];
   shellHook = ''
