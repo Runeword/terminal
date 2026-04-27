fe705d79cf15cada709aaabed7de69f702e1e41f
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-27 08:48:55 +0200

    Update wrappers/fd.nix

--- wrappers/fd.nix
+++ wrappers/fd.nix
@@ -1,13 +1,39 @@
-{ pkgs, files }:
+{
+  pkgs,
+  files,
+  tests,
+}:
 
-pkgs.symlinkJoin {
-  name = "fd-with-config";
-  paths = [ pkgs.fd ];
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  postBuild = ''
-    ${files.sync "ignore" ".config/ignore"}
+let
+  self = pkgs.symlinkJoin {
+    name = "fd-with-config";
+    paths = [ pkgs.fd ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      ${files.sync "ignore" ".config/ignore"}
 
-    wrapProgram $out/bin/fd \
-      --add-flags "--ignore-file $out/.config/ignore"
-  '';
-}
+      wrapProgram $out/bin/fd \
+        --add-flags "--ignore-file $out/.config/ignore"
+    '';
+    passthru.tests.smoke = tests.smoke {
+      name = "fd";
+      description = "Verify fd respects the shared ignore file";
+      script = ''
+        mkdir -p $TMPDIR/fd-test/node_modules
+        echo "secret" > $TMPDIR/fd-test/node_modules/file.txt
+        echo "visible" > $TMPDIR/fd-test/visible.txt
+
+        fd_output=$(${self}/bin/fd . $TMPDIR/fd-test 2>/dev/null)
+        echo "  files found:"
+        echo "$fd_output" | sed 's/^/    /'
+
+        if echo "$fd_output" | grep -q "node_modules"; then
+          fail "node_modules not ignored"
+        else
+          ok "node_modules ignored"
+        fi
+      '';
+    };
+  };
+in
+self
