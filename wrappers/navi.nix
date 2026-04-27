9b6784e687145c70456320b0fc8cc760af15a46a
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-27 08:48:55 +0200

    Update wrappers/navi.nix

--- wrappers/navi.nix
+++ wrappers/navi.nix
@@ -1,14 +1,33 @@
-{ pkgs, files }:
+{
+  pkgs,
+  files,
+  tests,
+}:
 
-pkgs.symlinkJoin {
-  name = "navi-with-config";
-  paths = [ pkgs.navi ];
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  postBuild = ''
-    ${files.sync "navi" ".config/navi"}
+let
+  self = pkgs.symlinkJoin {
+    name = "navi-with-config";
+    paths = [ pkgs.navi ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      ${files.sync "navi" ".config/navi"}
 
-    wrapProgram $out/bin/navi \
-      --set NAVI_CONFIG "$out/.config/navi/config.yaml" \
-      --set NAVI_PATH "$out/.config/navi"
-  '';
-}
+      wrapProgram $out/bin/navi \
+        --set NAVI_CONFIG "$out/.config/navi/config.yaml" \
+        --set NAVI_PATH "$out/.config/navi"
+    '';
+    passthru.tests.smoke = tests.smoke {
+      name = "navi";
+      description = "Verify navi loads its config";
+      script = ''
+        # navi info config-path shows the active config path; fails on bad config.
+        if ${self}/bin/navi info config-path > /dev/null 2>&1; then
+          ok "config loads"
+        else
+          fail "config failed to load"
+        fi
+      '';
+    };
+  };
+in
+self
