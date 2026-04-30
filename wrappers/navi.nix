799bd5bd94aa229ac8563bcf5558ff940288b4bf
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-30 22:28:46 +0200

    Update wrappers/navi.nix

--- wrappers/navi.nix
+++ wrappers/navi.nix
@@ -5,13 +5,20 @@
 }:
 
 let
+  config = files.mkConfig "navi-config" [
+    {
+      source = "navi";
+      target = ".config/navi";
+    }
+  ];
   self = pkgs.symlinkJoin {
     name = "navi-with-config";
-    paths = [ pkgs.navi ];
+    paths = [
+      pkgs.navi
+      config
+    ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      ${files.sync "navi" ".config/navi"}
-
       wrapProgram $out/bin/navi \
         --set NAVI_CONFIG "$out/.config/navi/config.yaml" \
         --set NAVI_PATH "$out/.config/navi"
