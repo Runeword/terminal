4f12dcaeeb553612a1fa03035619b30f52fb23f7
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-03 17:57:58 +0200

    Update devshells/terminal.nix

--- devshells/terminal.nix
+++ devshells/terminal.nix
@@ -2,11 +2,11 @@
 pkgs.mkShell {
   buildInputs = [
     (pkgs.writeShellScriptBin "h" ''
-      echo "type 'dev' to run alacritty in development mode"
-      echo "type 'bdl' to run alacritty in bundled mode"
-      echo "type 'tools' <name> to run a binary from the tools package"
-      echo "type 'smoke' to run wrapper smoke tests"
-      echo "type 'h' for help"
+      echo "'dev' run alacritty in development mode"
+      echo "'bdl' run alacritty in bundled mode"
+      echo "'tools' <name> run a binary from the tools package"
+      echo "'smoke' run wrapper smoke tests"
+      echo "'h' for help"
     '')
     (pkgs.writeShellScriptBin "dev" ''
       TERMINAL_CONFIG_DIR="$PWD/config" nix run .#dev --impure -- "$@"
