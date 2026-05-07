3f9a26b4ef5796a34c26a04d3dcf8678c2320fa6
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-07 23:20:48 +0200

    Update overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,5 @@
 { pkgs-24-05, pkgs-25-11 }:
 [
-  (final: prev: {
-    awscli2 = pkgs-24-05.awscli2;
-    tmux = pkgs-25-11.tmux;
-  })
-  (final: prev: {
-    firebase-tools = prev.firebase-tools.override {
-      buildNpmPackage = prev.buildNpmPackage.override {
-        nodejs = prev.nodejs_20;
-      };
-    };
-  })
+  (import ./channel-pins.nix { inherit pkgs-24-05 pkgs-25-11; })
+  (import ./firebase-tools.nix)
 ]
