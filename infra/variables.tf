2d626491997b6ca48feb9e1862b406ec6012613b
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-22 23:48:31 +0200

    Add infra/variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "github_owner" {
+  type        = string
+  default     = "Runeword"
+  description = "GitHub user or org that owns the repository."
+}
+
+variable "repository_name" {
+  type        = string
+  default     = "terminal"
+  description = "Repository to manage."
+}
