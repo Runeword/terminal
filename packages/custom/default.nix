2062986e19fa0973cac0555ca398e41a92a32fb7
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-24 23:21:05 +0200

    Update packages/custom/default.nix

--- packages/custom/default.nix
+++ packages/custom/default.nix
@@ -5,6 +5,6 @@
     pname = "git-branches";
     version = "0.1.0";
     src = ./git-branches;
-    vendorHash = null;
+    vendorHash = "sha256-uqVw/+79vkCQCF4QdP5LIo8CWdUoXRDaWFhYwr5QbT4=";
   })
 ]
