2ba5e786210dd9154c5c0251beb1fc63cd01b008
Runeword <60324746+Runeword@users.noreply.github.com>
2026-08-14 20:39:01 +0200

    Update sources/.config/shell/aliases.sh

--- sources/.config/shell/aliases.sh
+++ sources/.config/shell/aliases.sh
@@ -28,9 +28,7 @@ alias f='fzf --reverse --cycle --prompt=" " --height 70% --no-separator --info=i
 alias ss='systemctl --type=service --state=running | fzf --reverse --cycle --prompt=" " --height 70% --no-separator --info=inline:"" --header-lines=1'
 alias me='__open_device'
 alias bu='__bitwarden_unlock'
-# `env` alone pretty-prints the environment through the pager; with arguments it
-# must fall through to the real env so `env VAR=value cmd` still works.
-env() { if [ "$#" -eq 0 ]; then command env | f; else command env "$@"; fi; }
+envs() { command env | f; }
 alias k='__kill_processes'
 alias y='__yazi_cd < $TTY'
 
@@ -131,6 +129,7 @@ alias cheat='navi --cheatsh'
 alias tldr='navi --tldr'
 alias jira='JIRA_API_TOKEN="$(pass show JIRA_API_TOKEN)" jira'
 alias gh='GH_TOKEN="$(pass show GH_TOKEN)" gh'
+alias cachix='CACHIX_AUTH_TOKEN="$(pass show cachix/authtoken-main)" cachix'
 alias n='nvim'
 alias vi='vi -c "set number"'
 alias db='setsid appimage-run $HOME/AppImages/Chat2DB-Local-latest.AppImage'
